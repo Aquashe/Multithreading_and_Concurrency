d7f736bd81e4ef61b0eb685aaaa3f0f6569bd152
Aquashe <thomasukuttybenny2709@gmail.com>
2024-12-06 13:15:55 +0530

    Static Keyword file is added

---
+++ Java-Revision_Materials/StaticKeyword.java
@@ -0,0 +1,44 @@
+public class StaticKeyword {
+    public static class Emp{
+        String name;
+        int id ;
+        int salary;
+        static String ceo;
+        Emp(){
+            this.id = 1;
+            this.salary=1000;
+            this.name="not verified";
+        }
+        Emp(String name,int id , int salary,String ceo){
+            this.id = id;
+            this.salary=salary;
+            this.name = name;
+            Emp.ceo = ceo;
+
+        }
+
+        static{
+            ceo = "Larry";
+        }
+
+        public void show()
+        {
+            System.out.println("Name : "+name+" Id : "+id+" Salary :"+salary+" CEO : "+ceo);
+        }
+    }
+
+    public static void main(String[] args) {
+        Emp obj = new Emp();
+        Emp navin = new Emp("Navin",1,30000,"Lakshmi");
+        Emp Krishna = new Emp("Krishna",2,40000,"Ramesh");
+
+        Emp.ceo = "Thomas";
+        
+        obj.show();
+        navin.show();
+        Krishna.show();
+
+        Emp.ceo = "Thomas";
+
+    }
+}
